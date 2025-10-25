f0068d884340314d6363e88d683e09c0f6a59bd4
Om Prakash <omprakashmaurya268@gmail.com>
2025-10-25 23:22:11 +0530

    material ui added

---
+++ src/app/components/Pages/user/user-modal/user-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-modal',
+  imports: [],
+  templateUrl: './user-modal.component.html',
+  styleUrl: './user-modal.component.scss'
+})
+export class UserModalComponent {
+
+}
--- src/app/components/Pages/user/user.component.ts
+++ src/app/components/Pages/user/user.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { RoleService } from '../../../services/role.service';
 import { Router } from '@angular/router';
 import { User } from '../../../models/model';
 import { CommonModule } from '@angular/common';
 import { HasfeatureDirective } from '../../../directives/hasfeature.directive';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({ 
   selector: 'app-user',
-  imports: [CommonModule,HasfeatureDirective],
+  imports: [CommonModule,HasfeatureDirective,MatTableModule,MatPaginatorModule,MatButtonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  users: User[] = [];
   roleMap: Record<string, string> = {};
+  displayedColumns: string[] = ['position','name', 'username', 'role', 'action'];
+  dataSource:any
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     private userService: UserService,
@@ -24,7 +31,11 @@ export class UserComponent {
     this.roleService
       .getAllRoles()
       .forEach((r) => (this.roleMap[r.id] = r.name));
-    this.users = this.userService.getAllUsers();
+    this.dataSource = new MatTableDataSource<User>(this.userService.getAllUsers());
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   add() {
@@ -35,6 +46,6 @@ export class UserComponent {
   }
   delete(id: string) {
     this.userService.deleteUser(id);
-    this.users = this.userService.getAllUsers();
+    this.dataSource = this.userService.getAllUsers();
   }
 }
